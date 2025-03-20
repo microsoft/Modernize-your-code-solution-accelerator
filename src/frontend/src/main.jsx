626b5e144e62e19493f25b174068ef93c7b60a3d
Francia Riesco <friesco@microsoft.com>
2025-03-20 13:02:58 -0400

    Update main.jsx

--- src/frontend/src/main.jsx
+++ src/frontend/src/main.jsx
@@ -12,7 +12,12 @@ import { initializeMsalInstance } from './msal-auth/msalInstance';
 const Main = () => {
   const [isConfigLoaded, setIsConfigLoaded] = useState(false);
   const [msalInstance, setMsalInstance] = useState(null);
-  
+  const toBoolean = (value) => {
+    if (typeof value !== 'string') {
+      return false;
+    }
+    return value.trim().toLowerCase() === 'true';
+  };
   const [config, setConfig] = useState(null);
   useEffect(() => {
     const initMsal = async () => {
@@ -26,24 +31,25 @@ const Main = () => {
           REACT_APP_MSAL_POST_REDIRECT_URL: "",
           ENABLE_AUTH: true,
         };
-  
+
         if (response.ok) {
           config = await response.json();
+          config.ENABLE_AUTH = toBoolean(config.ENABLE_AUTH);
         }
-  
+
         window.appConfig = config;
         setEnvData(config);
         setApiUrl(config.API_URL);
         setConfig(config);
         // Wait for MSAL to initialize before setting state
-        const instance = await initializeMsalInstance(config);
+        const instance = config.ENABLE_AUTH ? await initializeMsalInstance(config) : {};
         setMsalInstance(instance);
         setIsConfigLoaded(true);
       } catch (error) {
         console.error("Error fetching config:", error);
       }
     };
-  
+
     initMsal(); // Call the async function inside useEffect
   }, []);
   async function checkConnection() {
@@ -52,7 +58,7 @@ const Main = () => {
     const baseURL = config.API_URL.replace(/\/api$/, ''); // Remove '/api' if it appears at the end
     console.log('Checking connection to:', baseURL);
     try {
-        const response = await fetch(`${baseURL}/health`);
+      const response = await fetch(`${baseURL}/health`);
     } catch (error) {
       console.error('Error connecting to backend:', error);
     }
@@ -70,12 +76,13 @@ const Main = () => {
     <StrictMode>
       <Provider store={store}>
         <FluentProvider theme={webLightTheme}>
-          {config && config.ENABLE_AUTH  ? (
-          <AuthProvider msalInstance={msalInstance}>
-            <App />
-          </AuthProvider>
-          ) : ( 
-            
+          {config && config.ENABLE_AUTH ? (
+
+            <AuthProvider msalInstance={msalInstance}>
+              <App />
+            </AuthProvider>
+          ) : (
+
             <App />
           )}
         </FluentProvider>
