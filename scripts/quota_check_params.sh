947b35128c84c872bcb00bd2cee83e09dee7c6b6
Roopan-Microsoft <168007406+Roopan-Microsoft@users.noreply.github.com>
2025-04-17 19:40:54 +0530

    Update quota_check_params.sh

--- scripts/quota_check_params.sh
+++ scripts/quota_check_params.sh
@@ -46,7 +46,7 @@ log_verbose() {
 }
 
 # Default Models and Capacities (Comma-separated in "model:capacity" format)
-DEFAULT_MODEL_CAPACITY="gpt-4o:30,gpt-4:30"
+DEFAULT_MODEL_CAPACITY="gpt-4o:5"
 # Convert the comma-separated string into an array
 IFS=',' read -r -a MODEL_CAPACITY_PAIRS <<< "$DEFAULT_MODEL_CAPACITY"
 
