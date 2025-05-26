68474d9e2e30bd7465b170d215e7aade58f2f9cb
Priyanka-Microsoft <v-prisinghal@microsoft.com>
2025-05-26 11:23:06 +0530

    pylint issue

--- src/backend/api/api_routes.py
+++ src/backend/api/api_routes.py
@@ -2,14 +2,24 @@
 
 import asyncio
 import io
-import zipfile
-import os
-import logging
 from typing import Optional
+# Standard library
+import logging
+import os
+import zipfile
 
+# Third-party
+from azure.monitor.opentelemetry import configure_azure_monitor
+from fastapi.responses import Response
+from opentelemetry import trace
+from opentelemetry.trace import Status, StatusCode
 
+# Local imports
 from api.auth.auth_utils import get_authenticated_user
+from api.event_utils import track_event_if_configured
 from api.status_updates import app_connection_manager, close_connection
+from sql_agents.process_batch import process_batch_async
+
 
 from common.logger.app_logger import AppLogger
 from common.services.batch_service import BatchService
@@ -27,12 +37,7 @@ from fastapi import (
 from fastapi.responses import Response
 
 from api.auth.auth_utils import get_authenticated_user
-from api.status_updates import app_connection_manager, close_connection
 from api.event_utils import track_event_if_configured
-from azure.monitor.opentelemetry import configure_azure_monitor
-from opentelemetry import trace
-from opentelemetry.trace import Status, StatusCode
-from sql_agents.process_batch import process_batch_async
 
 router = APIRouter()
 logger = AppLogger("APIRoutes")
